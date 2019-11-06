ad1472556ce0029d1dbcc400ec73deda63551aba
zoran-horvat <zh@codinghelmet.com>
2019-11-06 11:15:25 +0100

    Simplified XML snippets processing
    Used XmlDocument instead of XPathDocument to make extraction of snippet number and code simpler.

--- Source/LiveDemoRunner/Models/XmlSnippet.cs
+++ Source/LiveDemoRunner/Models/XmlSnippet.cs
@@ -10,9 +10,11 @@ namespace LiveDemoRunner.Models
         public int Number => this.ParseNumber();
         public string Code => this.ParseCode();
 
-        private XPathDocument Document { get; }
+        private XmlDocument Document { get; }
+        private XmlNamespaceManager Namespaces { get; }
 
         private static readonly string XpathNamespacePrefix = "x";
+        private static readonly string SnippetsNamespace = @"http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet";
         private static readonly string ShortcutXpath = @"//x:CodeSnippets/x:CodeSnippet/x:Header/x:Shortcut/text()";
         private static readonly string ShortcutNumberPattern = @"^snp(?<number>\d+)$";
         private static readonly string CodeXpath = @"//x:CodeSnippets/x:CodeSnippet/x:Snippet/x:Code/text()";
@@ -22,33 +24,26 @@ namespace LiveDemoRunner.Models
             loadFrom.Position = 0;
             using (XmlReader reader = new XmlTextReader(loadFrom))
             {
-                this.Document = new XPathDocument(reader);
+                this.Document = new XmlDocument();
+                this.Document.Load(reader);
+
+                this.Namespaces = new XmlNamespaceManager(this.Document.NameTable);
+                this.Namespaces.AddNamespace(XpathNamespacePrefix, SnippetsNamespace);
             }
         }
 
-        private string EvaluateXpath(XPathDocument document, string xpath, string namespacePrefix)
-        {
-            XPathNavigator navigator = document.CreateNavigator();
-            XPathExpression query = navigator.Compile(xpath);
-            XmlNamespaceManager manager = new XmlNamespaceManager(navigator.NameTable);
-            manager.AddNamespace(XpathNamespacePrefix, "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet");
-            query.SetContext(manager);
-            XPathNavigator result = navigator.SelectSingleNode(query);
-            return result?.Value ?? string.Empty;
-        }
-
         private int ParseNumber() =>
-            this.ParseNumber(this.EvaluateXpath(this.Document, ShortcutXpath, XpathNamespacePrefix));
+            this.ParseNumber(
+                this.Document.SelectSingleNode(ShortcutXpath, this.Namespaces)?.Value ?? string.Empty);
 
-        private int ParseNumber(string shortcut)
-        {
-            Match number = Regex.Match(shortcut, ShortcutNumberPattern);
-            if (number.Success && int.TryParse(number.Groups["number"].Value, out int value))
-                return value;
-            return -1;
-        }
+        private int ParseNumber(string shortcut) =>
+            Regex.Match(shortcut, ShortcutNumberPattern) is Match match &&
+            match.Success &&
+            int.TryParse(match.Groups["number"].Value, out int value)
+                ? value
+                : -1;
 
         private string ParseCode() =>
-            this.EvaluateXpath(this.Document, CodeXpath, XpathNamespacePrefix);
+            this.Document.SelectSingleNode(CodeXpath, this.Namespaces)?.Value ?? string.Empty;
     }
 }
