d9cc6bcfdf220cd445220fbcaee8e67558ada199
zoran-horvat <zh@codinghelmet.com>
2019-11-08 11:43:44 +0100

    Improved code readability

--- Source/LiveCoderExtension/Scripting/Parsing/Patterns/Snippet.cs
+++ Source/LiveCoderExtension/Scripting/Parsing/Patterns/Snippet.cs
@@ -14,16 +14,20 @@ namespace LiveCoderExtension.Scripting.Parsing.Patterns
             this.ApplyBeforePreamble(current, script);
 
         private Option<(IText remaining, DemoScript script)> ApplyBeforePreamble(NonEmptyText current, DemoScript script) =>
-            from preamble in new Regex(PreamblePattern).TryExtract(current.CurrentLine).Map(groups => (number: groups["number"].Value, terminator: groups["terminator"].Value))
+            from preamble in this.TryExtractPreamble(current)
             from text in current.ConsumeLine().OfType<NonEmptyText>()
             from record in this.ApplyAfterPreamble(text, preamble.number, preamble.terminator, script)
             select record;
 
+        private Option<(string number, string terminator)> TryExtractPreamble(NonEmptyText text) =>
+            new Regex(PreamblePattern)
+                .TryExtract(text.CurrentLine)
+                .Map(groups => (number: groups["number"].Value, terminator: groups["terminator"].Value));
+
         private Option<(IText remaining, DemoScript script)> ApplyAfterPreamble(NonEmptyText current, string snippetNumber, string terminator, DemoScript script) =>
-            current.ConsumeUntilMatch(this.EndingIn(terminator))
-                .Map(pair => (
-                    remaining: pair.remaining, 
-                    script: this.Apply(snippetNumber, this.RemoveTerminator(pair.lines, terminator), script)));
+            current.ConsumeUntilMatch(this.EndingIn(terminator)).Map(pair => (
+                remaining: pair.remaining, 
+                script: this.Apply(snippetNumber, this.RemoveTerminator(pair.lines, terminator), script)));
 
         private DemoScript Apply(string snippetNumber, string[] content, DemoScript script) => 
             script;
