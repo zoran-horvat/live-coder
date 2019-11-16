98d0be650ad900189b3555c83b054b879824f849
zoran-horvat <zh@codinghelmet.com>
2019-11-17 00:52:49 +0100

    Deployer filters out build output directories
    When preparing for deployment, directories produced during build and all their subdirectories are being ignored. Also, direcotries starting with . (dot), e.g. .vs, are also ignored.

--- Source/LiveCoder.Deployer/Implementation/DirectoryBrowser.cs
+++ Source/LiveCoder.Deployer/Implementation/DirectoryBrowser.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using LiveCoder.Common.Optional;
 
 namespace LiveCoder.Deployer.Implementation
 {
     class DirectoryBrowser
     {
+        private string[] SkipNames { get; } = {"bin", "obj", "Release", "Debug"};
+        private string[] SkipPrefixes { get; } = {"."};
+
         private DirectoryInfo Directory { get; }
      
         private DirectoryBrowser(DirectoryInfo directory)
@@ -20,11 +24,27 @@ namespace LiveCoder.Deployer.Implementation
         public IEnumerable<FileInfo> GetAllFiles() =>
             this.GetAllDirectories().SelectMany(dir => dir.GetFiles());
 
+        private IEnumerable<DirectoryInfo> SubdirectoriesOf(DirectoryInfo directory) =>
+            this.Filter(directory)
+                .Map(SubdirectoriesOfValid)
+                .Reduce(Enumerable.Empty<DirectoryInfo>());
+
+        private IEnumerable<DirectoryInfo> SubdirectoriesOfValid(DirectoryInfo directory) =>
+            new[] {directory}
+                .Concat(directory.GetDirectories().SelectMany(SubdirectoriesOf));
+
         private IEnumerable<DirectoryInfo> GetAllDirectories() =>
             this.SubdirectoriesOf(this.Directory);
 
-        private IEnumerable<DirectoryInfo> SubdirectoriesOf(DirectoryInfo directory) =>
-            new[] {directory}
-                .Concat(directory.GetDirectories().SelectMany(SubdirectoriesOf));
+        private Option<DirectoryInfo> Filter(DirectoryInfo directory) =>
+            this.IsInSkipNames(directory) ? Option.None<DirectoryInfo>()
+            : this.BeginsWithSkipPrefix(directory) ? Option.None<DirectoryInfo>()
+            : directory;
+
+        private bool IsInSkipNames(DirectoryInfo directory) =>
+            this.SkipNames.Contains(directory.Name, StringComparer.OrdinalIgnoreCase);
+
+        private bool BeginsWithSkipPrefix(DirectoryInfo directory) =>
+            this.SkipPrefixes.Any(prefix => directory.Name.StartsWith(prefix, StringComparison.OrdinalIgnoreCase));
     }
 }
