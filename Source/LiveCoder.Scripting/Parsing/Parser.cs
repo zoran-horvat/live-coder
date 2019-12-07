676aa9d76e5b226f30b692f760668673070be450
zoran-horvat <zh@codinghelmet.com>
2019-12-07 21:46:16 +0100

    Refactored parser

--- Source/LiveCoder.Scripting/Parsing/Parser.cs
+++ Source/LiveCoder.Scripting/Parsing/Parser.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using LiveCoder.Common;
 using LiveCoder.Common.Optional;
 using LiveCoder.Scripting.Lexing;
 using LiveCoder.Scripting.Lexing.Lexemes;
@@ -9,32 +10,39 @@ namespace LiveCoder.Scripting.Parsing
 {
     public class Parser
     {
+        private GrammarRules Grammar { get; }
+        private ParsingTable Table { get; }
+
+        public Parser()
+        {
+            this.Grammar = new GrammarRules();
+            this.Table = new ParsingTable(this.Grammar);
+        }
+
         public ScriptNode Parse(TokensArray tokens) =>
-            tokens.Any() ? this.ParseNonEmptyTree(tokens)
+            tokens.Any() ? this.Parse(tokens.GetAll().Concat(new[] {new EndOfInput()}))
             : Tree.ScriptNode.Empty;
 
-        private ScriptNode ParseNonEmptyTree(TokensArray tokens)
+        private ScriptNode Parse(IEnumerable<Token> tokens) =>
+            this.Parse(new ParsingStack(this.Table.Goto), tokens);
+
+        private ScriptNode Parse(ParsingStack stack, IEnumerable<Token> tokens) =>
+            Disposable.Using(tokens.GetEnumerator).Map(input => this.Parse(stack, input));
+
+        private ScriptNode Parse(ParsingStack stack, IEnumerator<Token> input)
         {
-            GrammarRules grammar = new GrammarRules();
-            ParsingTable table = new ParsingTable(grammar);
-            ParsingStack stack = new ParsingStack(table.Goto);
-
-            using (IEnumerator<Token> input = tokens.GetAll().Concat(new[] {new EndOfInput()}).GetEnumerator())
-            {
-                bool processInput = input.MoveNext();
-                while (processInput)
-                {
-                    if (stack.AcceptedResult is Some<ScriptNode> script)
-                        return script.Content;
-
-                    processInput = table
-                        .Shift(stack.StateIndex, input.Current)
-                        .Map(nextState => stack.Shift(input, nextState))
-                        .Reduce(() => table.Reduction(stack.StateIndex, input.Current).Map(stack.Reduce).Reduce(false));
-                }
-            }
-
-            return ScriptNode.Empty;
+            input.MoveNext();
+            while (stack.AcceptedResult is None<ScriptNode> && this.Advance(stack, input)) { }
+            return stack.AcceptedResult.Reduce(() => ScriptNode.Empty);
         }
+
+        private bool Advance(ParsingStack stack, IEnumerator<Token> input) =>
+            this.Shift(stack, input).Reduce(() => this.Reduce(stack, input));
+
+        private Option<bool> Shift(ParsingStack stack, IEnumerator<Token> input) =>
+            this.Table.Shift(stack.StateIndex, input.Current).Map(nextState => stack.Shift(input, nextState));
+
+        private bool Reduce(ParsingStack stack, IEnumerator<Token> input) =>
+            this.Table.Reduction(stack.StateIndex, input.Current).Map(stack.Reduce).Reduce(false);
     }
 }
