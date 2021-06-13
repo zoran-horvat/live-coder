ae9d99a58effdef14036544459cddb97b45efc66
Zoran Horvat <zh@codinghelmet.com>
2021-06-14 01:18:52 +0200

    Trimming message shown by the snippet

--- Source/LiveCoder.Snippets/RunningDemoSteps.cs
+++ Source/LiveCoder.Snippets/RunningDemoSteps.cs
@@ -57,8 +57,8 @@ namespace LiveCoder.Snippets
             {
                 (Match match, Func<StepSourceEntry, Option<RunningDemoSteps>> factory) = some.Content;
                 string shortcut = match.Groups["snippetShortcut"].Value;
-                string text = match.Groups["text"].Value.TrimEnd();
-                string code = line.Substring(0, match.Index);
+                string text = match.Groups["text"].Value.Trim();
+                string code = line.Substring(0, match.Index).TrimEnd();
                 StepSourceEntry entry = new StepSourceEntry(shortcut, lineIndex, text, code);
                 yield return (entry, factory);
 
